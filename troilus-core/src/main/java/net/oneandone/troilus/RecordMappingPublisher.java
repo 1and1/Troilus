763171531e560ad6d7e3f8d381e07a4c09a3935b
unknown <grro@LWKA-JH5WKX1.united.domain>
2015-03-09 19:00:44 +0100

    executeRx support

---
+++ troilus-core/src/main/java/net/oneandone/troilus/RecordMappingPublisher.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 1&1 Internet AG, https://github.com/1and1/
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.oneandone.troilus;
+
+import org.reactivestreams.Publisher;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+
+
+
+/**
+ *  RecordMappingPublisher
+ */
+class RecordMappingPublisher implements Publisher<Record> {
+    private final Publisher<net.oneandone.troilus.java7.Record> publisher;
+ 
+    /**
+     * @param publisher  the publisher to map
+     */
+    public RecordMappingPublisher(Publisher<net.oneandone.troilus.java7.Record> publisher) {
+        this.publisher = publisher;
+    }
+    
+    
+    @Override
+    public void subscribe(Subscriber<? super Record> subcriber) {
+        publisher.subscribe(new RecordMappingSubscriber(subcriber));
+    }
+    
+    
+    private static class RecordMappingSubscriber implements Subscriber<net.oneandone.troilus.java7.Record> {
+
+        private final Subscriber<? super Record> subcriber;
+        
+        public RecordMappingSubscriber(Subscriber<? super Record> subcriber) {
+            this.subcriber = subcriber;
+        }
+        
+        @Override
+        public void onSubscribe(Subscription subscription) {
+            subcriber.onSubscribe(subscription);
+        }
+        
+        @Override
+        public void onComplete() {
+            subcriber.onComplete();
+        }
+        
+        @Override
+        public void onError(Throwable t) {
+            subcriber.onError(t);
+        }
+        
+        @Override
+        public void onNext(net.oneandone.troilus.java7.Record record) {
+            subcriber.onNext(RecordAdapter.convertFromJava7(record));
+        }
+    }
+}
